1cf80722a068584c49b4214cf881e4ebb4e66791
mzoladki <mzoladki>
2022-12-06 09:26:18 +0000

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+print("Running Python!")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "HELLO WORLD";
+
+# COMMAND ----------
+
+# MAGIC %run ../Includes/Classroom-Setup-1.2
